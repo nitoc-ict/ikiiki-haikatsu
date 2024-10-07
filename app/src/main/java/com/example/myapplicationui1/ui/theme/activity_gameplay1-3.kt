cec6cb10a9fbf79921d57b20abce9207c953668c
Pranaria09 <eiju.chinen@gmail.com>
2024-10-07 23:50:45 +0900

    ゲーム画面のアスペクト比を修正

--- app/src/main/java/com/example/myapplicationui1/ui/theme/activity_gameplay1-3.kt
+++ app/src/main/java/com/example/myapplicationui1/ui/theme/activity_gameplay1-3.kt
@@ -1,4 +1,40 @@
 package com.example.myapplicationui1
 
-class GamePlay13Activity {
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.view.WindowManager.LayoutParams.SCREEN_ORIENTATION_CHANGED
+import com.unity3d.player.UnityPlayerActivity
+import com.unity3d.player.UnityPlayer
+
+class GamePlay13Activity: UnityPlayerActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gameplay13)
+        try {
+            mUnityPlayer = UnityPlayer(this as Activity)
+            findViewById<ConstraintLayout>(R.id.unity)?.addView(
+                mUnityPlayer, ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            )
+            mUnityPlayer.requestFocus()
+            window.clearFlags(SCREEN_ORIENTATION_CHANGED)
+            UnityPlayer.UnitySendMessage("SceneSelect", "ReceiveMessage", "AppleFarm")
+            Log.d("GamePlay13Activity", "はじめるわよ～")
+        } catch (e: Exception) {
+            Log.d("Error Try method", "${e}")
+        }
+
+    }
+    private fun returnSelectActivity() {
+        mUnityPlayer.onStop()
+        Log.d("GamePlay11Activity", "とめたわよ～")
+        val intent = Intent(this, ControllerSelectionActivity::class.java)
+        startActivity(intent)
+    }
 }
