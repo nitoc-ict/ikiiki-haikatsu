4a9940afc2fd60afb50ce3f91e676a2ce7e83bdb
Pranaria09 <eiju.chinen@gmail.com>
2024-10-12 11:29:42 +0900

    マイコンが複数接続できるように実装

--- app/src/main/java/com/example/myapplicationui1/ui/theme/activity_gameplay1-2.kt
+++ app/src/main/java/com/example/myapplicationui1/ui/theme/activity_gameplay1-2.kt
@@ -1,20 +1,92 @@
 package com.example.myapplicationui1
 
+import android.Manifest
 import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import android.view.WindowManager.LayoutParams.SCREEN_ORIENTATION_CHANGED
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myapplicationui1.ui.theme.ActivityEnd
 import com.unity3d.player.UnityPlayerActivity
 import com.unity3d.player.UnityPlayer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.io.OutputStream
+import java.util.UUID
+import java.util.concurrent.CountDownLatch
 
 class GamePlay12Activity: UnityPlayerActivity() {
+    // TAGs
+    private val TAG1 = "BluetoothAdapter"
+    private val TAG2 = "BluetoothConnectDevice"
+    private val TAG3 = "ReadDatafromBlutooth"
+    private val TAG4 = "FaildReadData"
+    private val TAG5 = "CloseConnection"
+
+    // About device information
+    private val DEVICE_NAME11 = "ESP32_11"
+    private val DEVICE_NAME12 = "ESP32_12"
+
+    private var devices = mutableListOf<BluetoothDevice>()
+    private val SPP_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+
+    // BluetoothAdapter接続
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    private var sockets = mutableListOf<BluetoothSocket>()
+
+    // BluetoothValue
+    private var PERMISSION_BLUETOOTH_CONNECT_CODE = (1)
+    private var BT_ONOFF_CONF = (3)
+
+    // readDataを管理する変数
+    private var isConnected: Boolean = false
+
+    private var stateSendValue: String = "0"
+
+    // ゲームの終了を知らせるフラグ
+    private var isfinishGame: Boolean = false
+
+    // CountDownLatch for synchronization
+    private val latch = CountDownLatch(1)
+
+    companion object {
+        private const val REQUEST_CODE_BLUETOOTH_CONNECT = 1
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == REQUEST_CODE_BLUETOOTH_CONNECT) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                connectToDevice()
+            } else {
+                Log.e(TAG1, "Required permission not granted")
+                finish()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gameplay12)
+        setContentView(R.layout.activity_gameplay21)
         try {
             mUnityPlayer = UnityPlayer(this as Activity)
             findViewById<ConstraintLayout>(R.id.unity)?.addView(
@@ -23,19 +95,231 @@ class GamePlay12Activity: UnityPlayerActivity() {
                     ViewGroup.LayoutParams.MATCH_PARENT
                 )
             )
+
+            // Broadcastを定義
+            val filter = IntentFilter().apply {
+                addAction(BluetoothDevice.ACTION_ACL_CONNECTED)
+                addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
+            }
+            registerReceiver(bluetoothReceiver, filter)
+
             mUnityPlayer.requestFocus()
             window.clearFlags(SCREEN_ORIENTATION_CHANGED)
-            UnityPlayer.UnitySendMessage("SceneSelect", "ReceiveMessage", "Pinpon")
-            Log.d("GamePlay12Activity", "はじめるわよ～")
+            setUpUnity()
+
         } catch (e: Exception) {
             Log.d("Error Try method", "${e}")
         }
-
     }
+
+    private fun setUpUnity() {
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                withContext(Dispatchers.IO) {
+                    UnityPlayer.UnitySendMessage("SceneSelect", "ReceiveMessage", "Wankosoba")
+                    latch.await()
+                }
+                Log.e(TAG1, "Finish async SceneSelect")
+                // bluetoothにマイコンが接続されていないとき、ゲームを停止して接続処理をする
+                if(bluetoothAdapter == null) {
+                    Log.e(TAG1, "Micon is not connecting")
+                    UnityPlayer.UnitySendMessage("WankosobaSystemManager", "PauseGame", "")
+                    reconnectToDevice()
+                } else {
+                    Log.d(TAG1, "Connected micon")
+                }
+            } catch (e: Exception){
+                Log.d(TAG1, "Error is: ", e)
+            }
+        }
+    }
+
+    // Unityで呼ぶ、latchの処理を進めるコールバック関数
+    fun onUnityMessageReceived() {
+        latch.countDown()
+    }
+
+    // Unity側で呼ぶ、別画面へ遷移する関数
     private fun returnSelectActivity() {
         mUnityPlayer.onStop()
-        Log.d("GamePlay12Activity", "とめたわよ～")
+        closeConnection()
+
+        isfinishGame = true
+        unregisterReceiver(bluetoothReceiver)
+
+        Log.d("GamePlay22Activity", "とめたわよ～")
         val intent = Intent(this, ActivityEnd::class.java)
         startActivity(intent)
+    }
+
+    private fun CheckPermissionBluetoothAdapter() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        Log.e(TAG1, "bluetoothAdapter is ${bluetoothAdapter}")
+
+        if(bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH),PERMISSION_BLUETOOTH_CONNECT_CODE) //リクエストパーミッションにbluetooth_connectを追加
+            }
+            startActivityForResult(enableBtIntent, BT_ONOFF_CONF)
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), REQUEST_CODE_BLUETOOTH_CONNECT)
+            CheckPermissionBluetoothAdapter()
+        } else {
+            Log.d(TAG1, "Permission Available 03")
+            connectToDevice()
+        }
+    }
+
+    private fun connectToDevice() {
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_GRANTED) {
+            try {
+                Log.d(TAG1, "Permission Available 04")
+                val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+
+                // 取得したデバイスをListに格納
+                pairedDevices?.forEach { device ->
+                    when(device.name) {
+                        DEVICE_NAME11 -> {
+                            Log.d(TAG1, "Device is ${device}")
+                            devices.add(device)
+                        }
+                        DEVICE_NAME12 -> {
+                            Log.d(TAG1, "Device is ${device}")
+                            devices.add(device)
+                        }
+                    }
+                }
+
+                // 取得したDeviceすべてをsocketに接続
+                devices.forEach { device ->
+                    if(device == null) {
+                        val socket = device.createRfcommSocketToServiceRecord(SPP_UUID)
+                        socket.connect()
+                        sockets.add(socket)
+                    }
+                }
+
+                Log.d(TAG1, "Devices is: ${sockets}")
+                UnityPlayer.UnitySendMessage("WankosobaSystemManager", "ResumeGame", "")
+
+                isConnected = true
+                readData()
+            } catch (e: Exception) {
+                isConnected = false
+                reconnectToDevice()
+            }
+        }
+    }
+
+    private fun readData() {
+        // マイコン毎にデータを送信
+        sockets.forEach { socket->
+            CoroutineScope(Dispatchers.IO).launch {
+                val inputStream: InputStream = socket.inputStream
+                val buffer = ByteArray(1024)
+
+                while(isConnected) {
+                    try {
+                        delay(85)
+                        val bytes = inputStream.read(buffer) ?: 0
+                        if(bytes > 0) {
+                            var incomingData = String(buffer, 0, bytes)
+                            Log.d(TAG3, "Rechieved: ${incomingData}")
+                            if (bytes != null) {
+                                stateSendValue = incomingData
+                                Log.d(TAG1, "incomingData is Null")
+                            } else {
+                                incomingData = stateSendValue
+                            }
+                            delay(300)
+                            UnityPlayer.UnitySendMessage("WankosobaSystemManager", "ReceiveMessage", "${incomingData}")
+                        }
+                    } catch (e: Exception) {
+                        Log.e(TAG4, "Read failed: ${e.message}")
+
+                        // 接続停止フラグを起動
+                        isConnected = false
+
+                        // PauseのメッセージをUnityに送信
+                        UnityPlayer.UnitySendMessage("WankosobaSystemManager", "PauseGame", "")
+                        reconnectToDevice()
+                    }
+                }
+            }
+        }
+    }
+
+    private val bluetoothReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val action = intent?.action
+            when(action) {
+                BluetoothDevice.ACTION_ACL_CONNECTED -> {
+                    Log.d(TAG1, "Bluetoothに再接続しました")
+                }
+                BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
+                    Log.d(TAG1, "Bluetoothが切断されました")
+                    UnityPlayer.UnitySendMessage("WankosobaSystemManager", "PauseGame", "")
+                    reconnectToDevice()
+                }
+            }
+        }
+    }
+
+    private fun reconnectToDevice() {
+        if (isfinishGame == false) {
+            CoroutineScope(Dispatchers.IO).launch {
+                delay(1000)
+                Log.d(TAG1, "Now Connecting...")
+                CheckPermissionBluetoothAdapter()
+            }
+        }
+    }
+
+    private fun closeConnection() {
+        // deviceすべてのSocketを停止
+        try {
+            sockets.forEach { socket ->
+                Log.d(TAG5, "Connection closed $socket")
+                socket.close()
+                Log.d(TAG5, "Connection closed")
+            }
+        } catch (e: Exception) {
+            Log.e(TAG5, "Error string connection: ${e.message}")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG1, "select onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG1, "select onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG1, "select onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG1, "select onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(bluetoothReceiver)
+        closeConnection()
     }
 }
